b6b9a992ca252759375fbba9fbeab248ea64233c
unknown <alehman26@basecampcodingacademy.org>
2025-09-17 11:59:58 -0500

    Start 3-Strings assignment

---
+++ 3-Strings/jasmine/src/main.js
@@ -0,0 +1,13 @@
+    // Task 1
+
+let line = "Stands so high";
+let line2 = "Huge hooves too";
+let line3 = "Impatiently waits for";
+
+// Takes the first letter of the string in the 'line' variable.
+let frontDoorLetter = line[0] + line2[0].toLowerCase() + line3[0].toLowerCase();
+console.log(frontDoorLetter)
+
+let frontDoorPassword;
+let backDoorLetter;
+let backDoorPassword;
