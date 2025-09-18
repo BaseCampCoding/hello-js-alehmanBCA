b3582b96d5743effb03c82e12ef7aad69e0f491f
unknown <alehman26@basecampcodingacademy.org>
2025-09-18 10:43:37 -0500

    Fix up 5-Arrays assignment.

--- 5-Arrays/jasmine/src/main.js
+++ 5-Arrays/jasmine/src/main.js
@@ -2,12 +2,13 @@
 let stack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log("Start Stack: " + stack);
 
-let position = 3; //index of the card you want to retrieve.
-let getItemResult = stack[position] + 1;
+let position = 9;
+let getItemResult = stack[position];
 console.log("Card: " + getItemResult);
 
 //Task 2
-let replacementCard = 6;
+position = 6;
+let replacementCard = 17;
 
 // Replaces the card at the index 'position' with the 
 // replacementCard variable.
@@ -17,37 +18,38 @@ console.log("Stack After Replacement: " + stack)
 console.log("(Card #" + stackBeforeReplacement + " Was Replaced with Card #" + replacementCard + ")");
 
 // Task 3
-let newCardTop = 5;
-stack[0] = newCardTop;
+let newCardTop = 25;
+stack.unshift(newCardTop);
 console.log("Stack With New Top: " + stack);
-console.log("(Top Card Replaced With Card #" + stack[0] + ")")
+console.log("(New Top Card #" + stack[0] + ")")
 
 // Task 4
-let cardToRemove = stack[position];
-stack.splice(cardToRemove, 1);
+position = 8
+let removedCard = stack[position]
+stack.splice(position, 1);
 console.log("Stack After Remove: " + stack);
-console.log("(Card #" + stack[position] + " Was Removed)")
+console.log("(Card #" + removedCard + " Was Removed)")
 
 // Task 5
 let deleteTopCard = stack[0];
-stack.splice(0, 1);
+stack.shift();
 console.log("Stack After Top Delete: " + stack);
 console.log("(Card #" + deleteTopCard + " Was Deleted.)");
 
 // Task 6
-let newCardBottom = 8;
+let newCardBottom = 18;
 stack.push(newCardBottom);
 console.log("Stack After New Bottom Card: " + stack);
 console.log("(Added Card #" + newCardBottom + " To The Bottom Of The Stack.)");
 
 // Task 7
-let removeBottomCard = stack.length - 1;
-stack.splice(removeBottomCard,1);
+let removeBottomCard = stack[stack.length - 1];
+stack.pop();
 console.log("New Stack: " + stack);
 console.log("(Deleted Card #" + removeBottomCard + " From The Bottom Of The Stack.)")
 
 // Task 8
-let stackSize = 5;
+let stackSize = 9;
 let checkSizeOfStackResult;
 console.log("Stack length is: " + stack.length);
 
