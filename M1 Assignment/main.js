1b84c448fb355d9ad73318aec7ce98da1f5a9317
unknown <alehman26@basecampcodingacademy.org>
2025-09-22 13:40:24 -0500

    Add the M1-Assignment folder and create the first function titled 'experience.'

---
+++ M1 Assignment/main.js
@@ -0,0 +1,9 @@
+let yearsOfExperience = 0;
+let levelOfExperience = '';
+
+//given a total number of years of study, returns the user's level of experience
+function experience(yearsOfExperience) {
+    if (yearsOfExperience >= 0 && yearsOfExperience <= 1) {
+        levelOfExperience = "Beginner";
+    }
+}
